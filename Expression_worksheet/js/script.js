a36321c5ade41328e7d34621ef0176e4c237b2c7
konceptjones <konceptgroup@gmail.com>
2014-08-13 22:29:49 -0400

    Expression practice

---
+++ Expression_worksheet/js/script.js
@@ -0,0 +1,10 @@
+//Calculate the area of a rectangle
+var width = 6;
+var height = 7;
+var area = width * height;
+console.log(area);
+
+//Calculate my dogs age in humman age
+var x = 7; //dog actual age
+var dogAge = 7; // Dogs first age in Human years 
+console.log(x);
