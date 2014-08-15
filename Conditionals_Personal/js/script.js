3db98a96216ba18d0c058461af3d7160041290e7
konceptjones <konceptgroup@gmail.com>
2014-08-14 23:48:45 -0400

    Conditionals Personal using Ternary operators.

---
+++ Conditionals_Personal/js/script.js
@@ -0,0 +1,8 @@
+//Michael Jones
+//August 21, 2014
+//Assignment: Conditionals Personal
+
+
+//The cost of purchasing an iPhone in is $199.99, otherwise if purchase online there is a $10 fee added for shipping.
+var iPhone = 199.99;
+var shipping = 10;
---
+++ Conditionals_Wacky/js/script.js
@@ -0,0 +1,14 @@
+//Michael Jones
+//August 14, 2014
+//Assignment: Expression
+
+//Mason Brick Measurments
+var a = Math.sqrt(0);
+var b = Math.sqrt(1);
+var c = Math.sqrt(9);
+var d = Math.sqrt(64);
+var e = Math.sqrt(-9);
+var masonBrick;
+brick = 15;
+masonCal = brick * c * d;
+console.log(masonCal);
