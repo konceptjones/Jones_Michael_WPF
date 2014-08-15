6479f157d8b0f6632f6f91db9564015ca5fb48f3
konceptjones <konceptgroup@gmail.com>
2014-08-14 22:09:39 -0400

    Expression Personal Assignment

---
+++ Expressions_Personal/js/script.js
@@ -0,0 +1,14 @@
+//Michael Jones
+//August 14, 2014
+//Assignment: Expressions
+
+//Health scope-JUST FOR TESTING
+var weight = prompt("Please enter your weight in lbs:");
+var height = prompt("Please enter your height in inches:");
+var age = prompt("Please enter your age:");
+var weight;
+weight = 150; // user weight
+var height;
+height = 65; // user height
+var BMI = weight / height;
+console.log(BMI);
---
+++ Expressions_Wacky/js/script.js
@@ -0,0 +1,10 @@
+//Michael Jones
+//August 14, 2014
+//Assignment: Expressions
+
+//The Sqaure ft. of a house
+var length = 20;
+var width = 16;
+var userInput = prompt("what type of room:");
+var area = length * width;
+console.log(area);
