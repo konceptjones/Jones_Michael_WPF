715e5a961edc54ea3152df4cbb795790378acd31
konceptjones <konceptgroup@gmail.com>
2014-08-21 14:45:55 -0400

    Delete script.js

--- Conditionals_Personal/js/script.js
+++
@@ -1,8 +0,0 @@
-//Michael Jones
-//August 21, 2014
-//Assignment: Conditionals Personal
-
-
-//The cost of purchasing an iPhone in is $199.99, otherwise if purchase online there is a $10 fee added for shipping.
-var iPhone = 199.99;
-var shipping = 10;
