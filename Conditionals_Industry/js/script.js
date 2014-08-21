2f0f282f21c2372a05d6744cf91b1bee6588405f
konceptjones <konceptgroup@gmail.com>
2014-08-21 15:43:48 -0400

    Conditionals Industry

---
+++ Conditionals_Industry/js/script.js
@@ -0,0 +1,12 @@
+//Michael Jones
+//August 21, 2014
+//Assignment: Conditionals - Industry
+
+//Work Force Digital ID For Workers
+var Firstname = prompt("Please enter First name");
+var Lastname = prompt("Please enter Last Name");
+var number = prompt("Please enter your unique number for ID"); // this number will be assigned to the employee name.
+if(answer = Firstname + Lastname + number){
+    //print employee unique ID
+    console.log(answer);
+}
